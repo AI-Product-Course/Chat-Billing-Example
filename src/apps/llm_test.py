cd6b5cabc120c04b21da9f8672fd8d50f0290b14
Daniil-Solo <daniil.solo1723@gmail.com>
2025-05-23 19:46:58 +0500

    hotfix after service schema changing

--- src/apps/llm_test.py
+++ src/apps/llm_test.py
@@ -1,10 +1,10 @@
 import asyncio
-from src.services.llm import OllamaLLMService, QuestionDTO
+from src.services.llm import OllamaLLMService
 
 
 async def main():
     llm = OllamaLLMService(model_name="llama3.2:3b", ollama_base_url="http://localhost:11434")
-    result = await llm.execute(QuestionDTO(text="Hi, how are you?", history=[]))
+    result = await llm.execute(text="Hi, how are you?", history=[])
     print(result)
 
 
